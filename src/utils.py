e99db41e835f6779717cd116a6147a1428e96654
isspek <ipekbrs@gmail.com>
2021-03-26 23:05:54 +0100

    add cross lang

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ RANDOM_SEED = 1234
 
 
 def data_args(parser):
-    parser.add_argument('--lang', type=str, help='Language', choices=['en', 'es'])
+    parser.add_argument('--lang', type=str, help='Language', choices=['en', 'es', 'en_es', 'es_en'])
     parser.add_argument('--data', type=str, help='Path to dataset')
     return parser
 
